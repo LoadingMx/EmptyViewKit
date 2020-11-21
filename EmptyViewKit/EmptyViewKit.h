60e1e4c9dc50ccb16a4f08100d697f8198a96a11
Mx <LoadingMx@126.com>
2020-11-21 17:29:53 +0800

    Initial Commit

---
+++ EmptyViewKit/EmptyViewKit.h
@@ -0,0 +1,18 @@
+//
+//  EmptyViewKit.h
+//  EmptyViewKit
+//
+//  Created by Mx on 2020/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmptyViewKit.
+FOUNDATION_EXPORT double EmptyViewKitVersionNumber;
+
+//! Project version string for EmptyViewKit.
+FOUNDATION_EXPORT const unsigned char EmptyViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyViewKit/PublicHeader.h>
+
+
